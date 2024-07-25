b852033abdb611cf3ad6ffaef3e5a5361004c1a5
santiago <santiagorg218@gmail.com>
2024-07-25 09:49:54 -0500

    se agregaron la funion de comentarios
    se creo el formulario para que los usuarios dejen sus comentarios y se manden  a la base de datos, se agrego un servicio para motrar de forma dinamica los comentarios

--- src/app/Home/dashboard.component.ts
+++ src/app/Home/dashboard.component.ts
@@ -1,22 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { MenuService } from '../../service/menuService';
 import { CommonModule } from '@angular/common';
-
+import { CommentsService } from '../../service/commnetService';
+import { Comments } from '../interface/comment.model';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [ CommonModule ],
+  imports: [ CommonModule, FormsModule ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent implements OnInit{
   featuredMenuItems: any[] = [];
+  testimonials: Comments[] = [];
+  newTestimonial = '';
 
-  constructor(private menuService: MenuService) {}
+  constructor(private menuService: MenuService, private comentsService: CommentsService) {}
 
   ngOnInit(): void {
     this.loadFeaturedMenuItems();
+    this.loadTestimonials();
   }
 
   loadFeaturedMenuItems() {
@@ -26,6 +31,31 @@ export class DashboardComponent implements OnInit{
       },
       error => {
         console.error('Failed to load featured menu items:', error);
+      }
+    );
+  }
+
+  loadTestimonials(): void {
+    this.comentsService.getTestimonials().subscribe(
+      (data) => this.testimonials = data,
+      (error) => console.error('Error fetching testimonials', error)
+    );
+  }
+
+  submitTestimonial(): void {
+    const userId = JSON.parse(localStorage.getItem('user') || '{}').id;
+    if (!this.newTestimonial.trim()) {
+      return;
+    }
+
+    this.comentsService.addTestimonial(userId, this.newTestimonial).subscribe(
+      () => {
+        this.newTestimonial = '';
+        this.loadTestimonials(); // Reload testimonials to show the new one
+      },
+      error => {
+        console.error('Failed to submit testimonial:', error);
+        alert('Failed to submit testimonial');
       }
     );
   }
